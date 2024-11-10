09a6d9550dc76a80b62d7e6022e871e6b409424c
jesiefab <112237226+jesiefab@users.noreply.github.com>
2024-11-10 10:43:38 +0000

    Created config

---
+++ htdocs/Stillwater_system/config.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    require_once "htdocs/GoogleAPI/vendor/autoload.php";
+    $gClient = new 
+    $gClient->setClientId("");
+    $gClient->setClientSecret("");
+    
+
+
+
+
+
+?>
--- htdocs/Stillwater_system/log_in.php
+++ htdocs/Stillwater_system/log_in.php
@@ -50,6 +50,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Add Client</title> 
 
     <script>
@@ -166,52 +168,107 @@
         <?php
             }
         ?>
-        
+        .divider:after,
+        .divider:before {
+        content: "";
+        flex: 1;
+        height: 1px;
+        background: #eee;
+        }
+        .h-custom {
+        height: calc(100% - 73px);
+        }
+        @media (max-width: 450px) {
+        .h-custom {
+        height: 100%;
+        }
+        }
     </style>
 
 </head>
 <body>
 
-    <div class="Back">
-        <button onclick="goBack()">Back</button>
-    </div>
+    <section class="vh-100">
+    <div class="container-fluid h-custom">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-md-9 col-lg-6 col-xl-5">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            class="img-fluid" alt="Sample image">
+        </div>
+        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <form>
+            <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                <p class="lead fw-normal mb-0 me-3">Sign in with</p>
+                <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-floating mx-1">
+                <i class="fab fa-facebook-f"></i>
+                </button>
 
-    <script>
-        // JavaScript to add hover effects to the add button
-        document.querySelector('button').addEventListener('mouseover', function() {
-            this.style.backgroundColor = '#45a049';
-            this.style.transform = 'scale(1.05)';
-        });
-        document.querySelector('button').addEventListener('mouseout', function() {
-            this.style.backgroundColor = '#4CAF50';
-            this.style.transform = 'scale(1)';
-        });
-    </script>
+                <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-floating mx-1">
+                <i class="fab fa-twitter"></i>
+                </button>
 
-    <div class="container">
-        <h1>Log In</h1>
+                <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-floating mx-1">
+                <i class="fab fa-google"></i>
+                </button>
+            </div>
 
-        <?php 
-            if ($failed) {
-                echo "<span>Incorrect Email or Password</span>";
-            }
-        ?>
+            <div class="divider d-flex align-items-center my-4">
+                <p class="text-center fw-bold mx-3 mb-0">Or</p>
+            </div>
 
-        <form action=" " method="POST">
+            <!-- Email input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="email" id="form3Example3" class="form-control form-control-lg"
+                placeholder="Enter a valid email address" />
+            </div>
 
-            <label for="email">Email:</label>
-            <input type="email" name="email" required>
+            <!-- Password input -->
+            <div data-mdb-input-init class="form-outline mb-3">
+                <input type="password" id="form3Example4" class="form-control form-control-lg"
+                placeholder="Enter password" />
+            </div>
 
-            <label for="password">Password:</label>
-            <input type="password" name="password" required>
+            <div class="d-flex justify-content-between align-items-center">
+                <!-- Checkbox -->
+                <div class="form-check mb-0">
+                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                <label class="form-check-label" for="form2Example3">
+                    Remember me
+                </label>
+                </div>
+                <a href="#!" class="text-body">Forgot password?</a>
+            </div>
 
-            <button type="submit" name="submit">Enter Account</button>
-        </form>
-        <div class="button-wrapper">
-            <button onclick="window.location.href='create_account.php'">
-                Create Account
-            </button>
+            <div class="text-center text-lg-start mt-4 pt-2">
+                <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+                style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+                <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
+                    class="link-danger">Register</a></p>
+            </div>
+
+            </form>
+        </div>
         </div>
     </div>
+    <div
+
+        <!-- Right -->
+        <div>
+        <a href="#!" class="text-white me-4">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="#!" class="text-white me-4">
+            <i class="fab fa-twitter"></i>
+        </a>
+        <a href="#!" class="text-white me-4">
+            <i class="fab fa-google"></i>
+        </a>
+        <a href="#!" class="text-white">
+            <i class="fab fa-linkedin-in"></i>
+        </a>
+        </div>
+        <!-- Right -->
+    </div>
+    </section>
 </body>
 </html>
